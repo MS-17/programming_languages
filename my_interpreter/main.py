ade87ef83c4d2b268f4e1db7cb6e3ec81b32b3aa
sergei <106600613+MS-17@users.noreply.github.com>
2023-12-03 11:10:03 +0800

    small change in main

--- my_interpreter/main.py
+++ my_interpreter/main.py
@@ -1,14 +1,11 @@
 from tokenizer import Tokenizer
 
 
-t = Tokenizer(" 4  +  6 ")
-print("Initial expression:", t)
+if __name__ == "__main__":
+	t = Tokenizer(" 4  +  6 ")
+	print("Initial expression:", t)
 
-tokens = t.get_tokens()
-print("\nTokenization result:")
-for i in tokens:
-	print(i)
-
-
-
-
+	tokens = t.get_tokens()
+	print("\nTokenization result:")
+	for i in tokens:
+		print(i)
